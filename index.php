058bc6c62ee21c4e6ceb5bb934357a4a06b3ca6c
moon2510 <62028865+moon2510@users.noreply.github.com>
2021-02-08 01:06:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -128,5 +128,23 @@
          </div>
    </div>
    <?php echo "Hello World!"; ?>
+   <script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '249684039898571',
+      xfbml      : true,
+      version    : 'v9.0'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 </body>
 </html>
